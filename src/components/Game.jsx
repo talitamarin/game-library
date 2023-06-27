bb85678ef94d4525c53cd3f4b6b9c997b70231b7
talitamarin <talita-smarin@outlook.com>
2023-06-26 21:27:14 -0300

    game

---
+++ src/components/Game.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+Game.protoType = {
+    title: PropTypes.string,
+    cover: PropTypes.string,
+    onRemove: PropTypes.func
+}
+export default function Game({ title, cover , onRemove }) {
+    return (
+        <div>
+          <img src={cover} alt="" />
+          <div>
+            <h2>{title}</h2>
+            <button onClick={onRemove}>Remover</button>
+          </div>
+          </div>
+    )
+}
